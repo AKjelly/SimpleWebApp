fd9629120519243457e2990487eb41c910ee4949
malik <malik@DESKTOP-IO3QK72>
2023-05-21 16:20:31 -0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SimpleWebApp.Data;
+using SimpleWebApp.Models;
+
+namespace SimpleWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ProductRepository _productRepository;
+
+        public HomeController(ProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        public IActionResult Index()
+        {
+            var products = _productRepository.GetAllProducts();
+            return View(products);
+        }
+
+        public IActionResult AddProduct(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _productRepository.AddProduct(product);
+                return RedirectToAction("Index");
+            }
+            return View(product);
+        }
+
+        //[HttpPost]
+        //public IActionResult AddProduct(Product product)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        _productRepository.AddProduct(product);
+        //        return RedirectToAction("Index");
+        //    }
+        //    return View(product);
+        //}
+
+//        public IActionResult EditProduct(int id)
+//        {
+//            var product = _productRepository.GetProductById(id)
+//;
+//            if (product == null)
+//            {
+//                return NotFound();
+//            }
+//            return View(product);
+//        }
+
+        //[HttpPost]
+        public IActionResult EditProduct(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _productRepository.UpdateProduct(product);
+                return RedirectToAction("Index");
+            }
+            return View(product);
+        }
+
+        public IActionResult DeleteProduct(int id)
+        {
+            var product = _productRepository.GetProductById(id)
+;
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return View(product);
+        }
+
+        [HttpPost]
+        public IActionResult DeleteProduct(Product product)
+        {
+            _productRepository.DeleteProduct(product.Id);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Data/ProductRepository.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using Dapper;
+using Microsoft.Extensions.Configuration;
+using SimpleWebApp.Models;
+
+namespace SimpleWebApp.Data
+{
+    public class ProductRepository
+    {
+        
+        private readonly IConfiguration _configuration;
+
+        public ProductRepository(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public IDbConnection Connection
+        {
+            get
+            {
+                return new SqlConnection(_configuration.GetConnectionString("DefaultConnection"));
+            }
+        }
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            using (IDbConnection conn = Connection)
+            {
+                string query = "SELECT * FROM Products";
+                return conn.Query<Product>(query);
+            }
+        }
+
+        public void AddProduct(Product product)
+        {
+            using (IDbConnection conn = Connection)
+            {
+                string query = "INSERT INTO Products (ProductName,Price,Description,CreateDate) VALUES (@ProductName,@Price,@Description,@CreateDate)";
+                conn.Execute(query, product);
+            }
+        }
+
+        public Product GetProductById(int id)
+        {
+            using (IDbConnection conn = Connection)
+            {
+                string query = "SELECT * FROM Products WHERE Id = @Id";
+                return conn.QueryFirstOrDefault<Product>(query, new { Id = id });
+            }
+        }
+        public void UpdateProduct(Product product)
+        {
+            using (IDbConnection conn = Connection)
+            {
+                string query = "UPDATE Products SET ProductName = @ProductName, Price = @Price,Description=@Description,CreateDate=@CreateDate WHERE Id = @Id";
+                conn.Execute(query, product);
+            }
+        }
+
+        public void DeleteProduct(int id)
+        {
+            using (IDbConnection conn = Connection)
+            {
+                string query = "DELETE FROM Products WHERE Id = @Id";
+                conn.Execute(query, new { Id = id });
+            }
+        }
+    }
+}
